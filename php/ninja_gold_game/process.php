78ff3eb227aa02afde7122497649ade037ca0c35
aresproject <aresproj3ct@gmail.com>
2020-02-20 19:59:18 +0800

    process.php code lines reduction

--- php/ninja_gold_game/process.php
+++ php/ninja_gold_game/process.php
@@ -1,30 +1,13 @@
 <?php
 session_start();
-function quest($place, $min, $max){
-  $_SESSION['gold'] = rand($min, $max);
-  return "<span style='color: green;'> You entered a " . $place . " and earned " .  $_SESSION['gold'] . " golds " . date('F jS Y g:i a')  ."</span> <br>";
-}
-if($_POST['action'] == 'farm') {
-  $pid[] = quest($_POST['action'], 10, 20);
-}
-elseif($_POST['action'] == 'cave') {
-  $pid[] = quest($_POST['action'], 5, 10);
-}
-elseif($_POST['action'] == 'house') {
-  $pid[] = quest($_POST['action'], 2, 5);
-}
-elseif($_POST['action'] == 'casino') {
-  $_SESSION['gold'] = rand(0,50);
-  $odds  = (rand(0,1) < 1) ? $_SESSION['gold'] *= -1 : $_SESSION['gold'];
-  if($odds > 0) {
-    $pid[] = "<span style='color: green;'> You entered a " .  $_POST['action'] . " and earned " .  $_SESSION['gold'] . " golds " . date('F jS Y g:i a')  ."</span> <br>";
-  }
-  else $pid[] = "<span style='color: red;'> You entered a " .  $_POST['action'] . " and lost " .  $_SESSION['gold'] . " golds... Ouch... " . date('F jS Y g:i a')  ."</span> <br>";
-}
+if($_POST['action'] == 'farm') $pid[] = "<span class='green'> You entered a " . $_POST['action'] . " and earned " .  $_POST['farm']  . " golds " . date('F jS Y g:ia') . "</span> <br>";
+elseif($_POST['action'] == 'cave') $pid[] = "<span class='green'> You entered a " . $_POST['action'] . " and earned " .  $_POST['cave']  . " golds " . date('F jS Y g:ia') . "</span> <br>";
+elseif($_POST['action'] == 'house') $pid[] = "<span class='green'> You entered a " . $_POST['action'] . " and earned " .  $_POST['house']  . " golds " . date('F jS Y g:ia') . "</span> <br>";
+if($_POST['action'] == 'casino' && $_POST['odds'] == 0) {
+  $_SESSION['gold'] = $_POST['casino']  *= -1;
+  $pid[] = "<span class='red'> You entered a " . $_POST['action'] . " and lost " . $_SESSION['gold'] . " golds... ouch... " . date('F jS Y g:i a') . "</span> <br>"; }
+elseif($_POST['action'] == 'casino' && $_POST['odds'] == 1) $pid[] = "<span  class='green'> You entered a " . $_POST['action'] . " and earned " . $_POST['casino']  . " golds " . date('F jS Y g:ia') ."</span> <br>";
 $_SESSION['Total_gold'] += $_SESSION['gold'];
 $_SESSION['results'][] = $pid;
-if(isset($_POST['action']) && $_POST['action'] == "reset"){
-  session_destroy();
-}
 header("Location: index.php");
 ?>
